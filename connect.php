bae894d94c902908b11edb032a46718453836035
fmarsaa <Fatuma.Marsa@strathmore.edu>
2024-09-25 18:45:57 +0300

    signup

--- connect.php
+++ connect.php
@@ -5,6 +5,7 @@ $dbname = 'signup';
 $username = 'root'; 
 $password = '';  
 
+try {
     
     $dsn = "mysql:host=$host;dbname=$dbname;charset=utf8";
     
@@ -13,5 +14,7 @@ $password = '';
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     echo "Connected successfully to the database!";
-
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
 ?>
