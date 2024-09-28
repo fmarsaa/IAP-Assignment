997714b90c3e415a3df0efe66be1086a9f1c5534
fmarsaa <Fatuma.Marsa@strathmore.edu>
2024-09-29 00:04:04 +0300

    display

--- connect.php
+++ connect.php
@@ -3,18 +3,14 @@
 require_once 'User.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $first_name = $_POST['first_name'];
-    $last_name = $_POST['last_name'];
-    $email = $_POST['email'];
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-
-    $database = new Database();
-    $db = $database->connect();
-    $user = new User($db);
+    // Previous code...
 
     if ($user->createUser($first_name, $last_name, $email, $username, $password)) {
         echo "User registered successfully!";
+        $users = $user->readUsers();
+        echo "<pre>";
+        print_r($users);
+        echo "</pre>";
     } else {
         echo "Failed to register user.";
     }
