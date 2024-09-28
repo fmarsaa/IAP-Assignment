952d7d00c150d0c5242711235ba5111d5b77216d
fmarsaa <Fatuma.Marsa@strathmore.edu>
2024-09-29 00:15:01 +0300

    implement

--- User.php
+++ User.php
@@ -10,6 +10,19 @@ class User {
     }
 
     public function createUser($first_name, $last_name, $email, $username, $password) {
+        // Validate email
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            throw new Exception("Invalid email format");
+        }
+
+        // Validate password length
+        if (strlen($password) < 8) {
+            throw new Exception("Password must be at least 8 characters long");
+        }
+
+        // Hash the password
+        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
         // Prepare SQL statement
         $sql = "INSERT INTO users (first_name, last_name, email, username, password) 
                 VALUES (:first_name, :last_name, :email, :username, :password)";
@@ -20,14 +33,16 @@ class User {
         $stmt->bindParam(':last_name', $last_name);
         $stmt->bindParam(':email', $email);
         $stmt->bindParam(':username', $username);
-        $stmt->bindParam(':password', $password);
+        $stmt->bindParam(':password', $hashed_password);
 
-        
+        // Execute the statement
         return $stmt->execute();
     }
 
     public function readUsers() {
-        
+        $sql = "SELECT * FROM users";
+        $stmt = $this->conn->query($sql);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 }
 
