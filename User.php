dff341c5d84202f72949e1e006d3a4ecd75057b3
fmarsaa <Fatuma.Marsa@strathmore.edu>
2024-09-29 00:11:44 +0300

    parameters

--- User.php
+++ User.php
@@ -10,12 +10,20 @@ class User {
     }
 
     public function createUser($first_name, $last_name, $email, $username, $password) {
-        
+        // Prepare SQL statement
         $sql = "INSERT INTO users (first_name, last_name, email, username, password) 
                 VALUES (:first_name, :last_name, :email, :username, :password)";
         $stmt = $this->conn->prepare($sql);
 
+        // Bind parameters
+        $stmt->bindParam(':first_name', $first_name);
+        $stmt->bindParam(':last_name', $last_name);
+        $stmt->bindParam(':email', $email);
+        $stmt->bindParam(':username', $username);
+        $stmt->bindParam(':password', $password);
+
         
+        return $stmt->execute();
     }
 
     public function readUsers() {
