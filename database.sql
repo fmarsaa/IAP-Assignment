bba059a6dbaae75b329d9112a45556d432b5183b
fmarsaa <Fatuma.Marsa@strathmore.edu>
2024-10-01 21:51:41 +0300

    sql

--- database.sql
+++ database.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS registration (
     id INT AUTO_INCREMENT PRIMARY KEY,
     fname VARCHAR(50) NOT NULL,
     lname VARCHAR(50) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE,
-    username VARCHAR(50) NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL ,
+    username VARCHAR(50) NOT NULL ,
     password VARCHAR(255) NOT NULL
 );
